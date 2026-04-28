c6b0bdd959c5b70c96d20e7de1613fb00938816d
Mohammad <mohammadhmada06@gmail.com>
2026-04-29 00:27:42 +0300

    bulid Mock Bank System

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import authRoutes from "./modules/auth/auth.routes";
+import mock_bank from "./modules/mockBank/mockBank.routes"
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/mock-bank", mock_bank)
 
 export default app;
