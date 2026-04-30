8d6de7f42072dc6a78550995c8d81e032f2fc6aa
Mohammad <mohammadhmada06@gmail.com>
2026-04-30 20:16:19 +0300

    frontend/memberScreen

--- frontend/src/api/family.api.ts
+++ frontend/src/api/family.api.ts
@@ -1,8 +1,34 @@
-import { apiRequest } from "./client";
-import type { MyFamilyResponse } from "../types/family.types";
+﻿import { apiRequest } from "./client";
+import type {
+  FamilyMembersResponse,
+  FamilyMembersStatsResponse,
+  MyFamilyResponse,
+} from "../types/family.types";
 
 export const getMyFamilyApi = async (): Promise<MyFamilyResponse> => {
   return apiRequest<MyFamilyResponse>("/api/families/my-family", {
     method: "GET",
   });
+};
+
+export const getFamilyMembersApi = async (
+  familyId: number
+): Promise<FamilyMembersResponse> => {
+  return apiRequest<FamilyMembersResponse>(
+    `/api/families/${familyId}/members`,
+    {
+      method: "GET",
+    }
+  );
+};
+
+export const getFamilyMembersStatsApi = async (
+  familyId: number
+): Promise<FamilyMembersStatsResponse> => {
+  return apiRequest<FamilyMembersStatsResponse>(
+    `/api/families/${familyId}/members/stats`,
+    {
+      method: "GET",
+    }
+  );
 };
